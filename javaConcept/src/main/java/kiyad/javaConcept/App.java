578aae7da5b1ce4b61b258a8e24bf7a43227698e
kuldeep <kuldeep@LAPTOP-3CLEMJEN>
2025-01-18 19:58:22 +0530

    added student class

--- javaConcept/src/main/java/kiyad/javaConcept/App.java
+++ javaConcept/src/main/java/kiyad/javaConcept/App.java
@@ -6,5 +6,8 @@ package kiyad.javaConcept;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello kiyansh");
+        
+        Student s = new Student();
+        s.show();
     }
 }
